433038c07bcff4c674cc4a88c99f50f830a88122
Lucas Berto <lucaum.berto@gmail.com>
2026-09-22 09:41:14 -0300

    chore: projeto movido para ~/Projects/TOOLS/database-backup

--- run-backup.sh
+++ run-backup.sh
@@ -6,7 +6,7 @@ set -uo pipefail
 
 MOUNTPOINT="/run/media/lucas/Dados"
 DEVICE="/dev/disk/by-uuid/01DCF4F5AAE8F170"   # partição NTFS "Dados" (nvme0n1p1)
-PROJECT_DIR="/home/lucas/Projects/GO/database-backup"
+PROJECT_DIR="/home/lucas/Projects/TOOLS/database-backup"
 LOG_DIR="$PROJECT_DIR/logs"
 
 mkdir -p "$LOG_DIR"
